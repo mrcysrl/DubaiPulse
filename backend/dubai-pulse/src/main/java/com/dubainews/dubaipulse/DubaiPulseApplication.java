63bc2f5ed4bd233c2ffbc040b64cb67714bd6491
gmznl <gomezproductionnl@gmail.com>
2026-01-05 11:57:00 +0400

    Setting Up Environment

---
+++ backend/dubai-pulse/src/main/java/com/dubainews/dubaipulse/DubaiPulseApplication.java
@@ -0,0 +1,13 @@
+package com.dubainews.dubaipulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubaiPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubaiPulseApplication.class, args);
+	}
+
+}
---
+++ backend/dubai-pulse/src/test/java/com/dubainews/dubaipulse/DubaiPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubainews.dubaipulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubaiPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
